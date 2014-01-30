1e4d842c140756e1e503afe5ff51cea5564d31c8
Aaron Dandy <aaron.dandy@live.com>
2014-01-29 21:10:52 -0700

    working on the require section

---
+++ CodeContracts/CCSnippets/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace CCSnippets
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var text = "test";
+            Console.WriteLine(NoNull(text));
+        }
+
+        static string NoNull(string input) {
+            Contract.Requires(input != null);
+            Contract.Ensures(Contract.Result<string>() != null);
+            return input;
+        }
+
+    }
+}
