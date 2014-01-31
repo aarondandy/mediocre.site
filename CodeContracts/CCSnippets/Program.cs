9b34e212349730b2c45cd9e03d43c65c8773898e
Aaron Dandy <aaron.dandy@live.com>
2014-01-31 08:41:21 -0700

    standard preconditions are for public surface

--- CodeContracts/CCSnippets/Program.cs
+++ CodeContracts/CCSnippets/Program.cs
@@ -8,12 +8,30 @@ namespace CCSnippets
         static void Main(string[] args) {
             var text = "test";
             Console.WriteLine(NoNull(text));
+
+            Console.WriteLine(WordTitleCase("woRd"));
+
+            Console.ReadKey();
         }
 
         static string NoNull(string input) {
             Contract.Requires(input != null);
             Contract.Ensures(Contract.Result<string>() != null);
             return input;
+        }
+
+        public static string WordTitleCase(string input) {
+            if (String.IsNullOrEmpty(input))
+                throw new ArgumentException("Must not be null or empty.", "input");
+            if (Char.IsWhiteSpace(input[0]) || Char.IsWhiteSpace(input[input.Length - 1]))
+                throw new ArgumentException("Must be trimmed.", "input");
+            Contract.EndContractBlock();
+
+            var builder = new System.Text.StringBuilder(input.Length);
+            builder.Append(Char.ToUpper(input[0]));
+            for (int i = 1; i < input.Length; i++)
+                builder.Append(Char.ToLower(input[i]));
+            return builder.ToString();
         }
 
     }
