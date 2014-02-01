f770e09e259e2cc6f285d275afec32d75aaee97d
Aaron Dandy <aaron.dandy@live.com>
2014-02-01 12:35:18 -0700

    working on better pre/post condition examples

---
+++ CodeContracts/CCSnippets/Matrix2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace CCSnippets
+{
+    public class Matrix2
+    {
+        public double E00;
+        public double E01;
+        public double E10;
+        public double E11;
+
+        public double CalculateDeterminant() {
+            return (E00 * E11) - (E10 * E01);
+        }
+
+        [Pure]
+        private static bool AreUnchanged(double then, double now) {
+            if (Double.IsNaN(then)) return Double.IsNaN(now);
+            if (Double.IsPositiveInfinity(then)) return Double.IsPositiveInfinity(now);
+            if (Double.IsNegativeInfinity(then)) return Double.IsNegativeInfinity(now);
+            return then == now;
+        }
+
+        public void Invert() {
+            Contract.EnsuresOnThrow<InvalidOperationException>(
+                AreUnchanged(Contract.OldValue(E00),E00)
+                && AreUnchanged(Contract.OldValue(E01),E01)
+                && AreUnchanged(Contract.OldValue(E10),E10)
+                && AreUnchanged(Contract.OldValue(E11),E11));
+
+            var determinant = CalculateDeterminant();
+            if (0 == determinant
+                || Double.IsNaN(determinant)
+                || Double.IsInfinity(determinant))
+                throw new InvalidOperationException();
+
+            var temp = E00;
+            E00 = E11 / determinant;
+            E11 = temp / determinant;
+            temp = -determinant;
+            E01 = E01 / temp;
+            E10 = E10 / temp;
+        }
+
+        public override string ToString() {
+            Contract.Ensures(!String.IsNullOrWhiteSpace(Contract.Result<string>()));
+            return String.Concat(E00, ' ', E01, "\n", E10, ' ', E11);
+        }
+
+    }
+}
--- CodeContracts/CCSnippets/Program.cs
+++ CodeContracts/CCSnippets/Program.cs
@@ -9,8 +9,35 @@ namespace CCSnippets
             var text = "test";
             Console.WriteLine(NoNull(text));
 
+            Contract.Assume(IsTrimmed("woRd"));
             Console.WriteLine(WordTitleCase("woRd"));
-            Console.WriteLine(new[]{1,2,3}.QuickFirstOrDefault());
+
+            var m = new Matrix2 {
+                E00 = 1,
+                E01 = 2,
+                E10 = 3,
+                E11 = Double.NaN,
+            };
+            try {
+                m.Invert();
+            }
+            catch (InvalidOperationException) {
+                Console.WriteLine("oops!");
+            }
+
+            Console.WriteLine(m);
+
+            var a = new Base("123");
+            a.Append("!");
+
+            Console.WriteLine(a.Value);
+
+            var b = new WithNewLines("123");
+            b.Append("!");
+            b.AppendRaw("abc");
+
+            Console.WriteLine(b.Value);
+
             Console.ReadKey();
         }
 
@@ -20,13 +47,24 @@ namespace CCSnippets
             return input;
         }
 
+        
+        [Pure]
+        public static bool IsTrimmed(string text) {
+            if (text == null)
+                return false;
+            if (text.Length == 0)
+                return true;
+            if (Char.IsWhiteSpace(text[0]))
+                return false;
+            return text.Length == 1 || !Char.IsWhiteSpace(text[text.Length - 1]);
+        }
+
         public static string WordTitleCase(string input) {
             if (String.IsNullOrEmpty(input))
                 throw new ArgumentException("Must not be null or empty.", "input");
-            if (Char.IsWhiteSpace(input[0]) || Char.IsWhiteSpace(input[input.Length - 1]))
+            if (!IsTrimmed(input))
                 throw new ArgumentException("Must be trimmed.", "input");
             Contract.EndContractBlock();
-
             var builder = new System.Text.StringBuilder(input.Length);
             builder.Append(Char.ToUpper(input[0]));
             for (int i = 1; i < input.Length; i++)
@@ -41,6 +79,44 @@ namespace CCSnippets
         internal static T QuickFirstOrDefault<T>(this T[] array) {
             Contract.Requires(array != null);
             return array.Length == 0 ? default(T) : array[0];
+        }
+    }
+
+
+    public class Base {
+        public string Value { get; set; }
+        public Base(string value) {
+            if (value == null)
+                throw new ArgumentNullException("value");
+            Contract.EndContractBlock();
+            Value = value;
+        }
+        public virtual void Append(string text) {
+            Contract.Requires(text != null);
+            Contract.EndContractBlock(); // a hint
+            // not a part of the contract block
+            // but will be in all builds to enforce.
+            // must be seperate because I am using
+            // Custom Parameter Validation
+            if (text == null)
+                throw new ArgumentNullException("text");
+            Value += text;
+        }
+    }
+
+    public class WithNewLines : Base {
+        public WithNewLines(string value)
+            : base(value) { // enforced in here
+            Contract.Requires(value != null);
+        }
+        public override void Append(string text) {
+            // inherits preconditions automatically
+            base.Append(text); // enforced in here
+            Value += Environment.NewLine;
+        }
+        public void AppendRaw(string text) {
+            Contract.Requires(text != null);
+            base.Append(text); // enforced in here
         }
     }
 
