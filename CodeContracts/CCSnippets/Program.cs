3c10edbeb581da9bfbfd5ba83d98f25e686919a0
Aaron Dandy <aaron.dandy@live.com>
2014-04-24 23:37:53 -0600

    finished up most of the inheritance stuff

--- CodeContracts/CCSnippets/Program.cs
+++ CodeContracts/CCSnippets/Program.cs
@@ -3,51 +3,87 @@ using System.Diagnostics.Contracts;
 
 namespace CCSnippets
 {
-class Program
-{
 
+class Program {
     static void Main(string[] args) {
-        var thing = new SmallThing();
-        var letter = thing.GetLetter();
-        Console.WriteLine(letter);
-        Console.ReadKey();
+        var sounder = new Screech();
+        Console.WriteLine(sounder.LoudNoise());
     }
-
 }
 
-public interface IThing
-{
-    char GetLetter(int n);
+[ContractClass(typeof(NoiseMakerContracts))]
+public abstract class NoiseMaker {
+    public virtual string LoudNoise() {
+        Contract.Ensures(Contract.Result<string>() != null);
+        return Noise().ToUpper();
+    }
+    public abstract string Noise();
 }
 
+[ContractClassFor(typeof(NoiseMaker))]
+internal abstract class NoiseMakerContracts : NoiseMaker {
+    public override string Noise() {
+        Contract.Ensures(Contract.Result<string>() != null);
+        throw new NotImplementedException();
+    }
+}
 
-public abstract class BaseThing
-{
+public class Screech : NoiseMaker{
+    public override string Noise() {
+        return "screeeeeeeech";
+    }
+}
 
-    public virtual int GetValue() {
+public class Nope : NoiseMaker {
+    public override string Noise() {
+        return null; // ensures is false: Contract.Result<string>() != null
+    }
+}
+
+[ContractClass(typeof(SomeBaseContracts))]
+public abstract class SomeBase {
+    public abstract string ImportantStuff(string input);
+    public abstract string NobodyCares();
+}
+
+[ContractClassFor(typeof(SomeBase))]
+internal abstract class SomeBaseContracts : SomeBase {
+    public override string ImportantStuff(string input) {
+        Contract.Requires(input != null);
+        Contract.Ensures(Contract.Result<string>() != null);
+        throw new NotImplementedException();
+    }
+    public abstract override string NobodyCares();
+}
+
+[ContractClass(typeof(SomeInterfaceContracts))]
+public interface ISomeInterface {
+    string TopValue(int n);
+}
+
+[ContractClassFor(typeof(ISomeInterface))]
+internal abstract class SomeInterfaceContracts : ISomeInterface {
+    public string TopValue(int n) {
+        Contract.Requires(n >= 0);
+        Contract.Ensures(Contract.Result<string>() != null);
+        throw new NotImplementedException();
+    }
+}
+
+[ContractClass(typeof(ExtraStuffContracts))]
+public interface IExtraStuff : ISomeInterface {
+    int GetNumber();
+}
+
+[ContractClassFor(typeof(IExtraStuff))]
+internal abstract class ExtraStuffContracts : IExtraStuff {
+    public int GetNumber() {
         Contract.Ensures(Contract.Result<int>() >= 0);
-        return 10;
+        return 0;
     }
-
-    public abstract char GetLetter(int n);
+    public abstract string TopValue(int n);
 }
 
-public class SmallThing : IThing
-{
-    public int GetValue() {
-        Contract.Ensures(Contract.Result<int>() < 4);
-        return 3;
-    }
 
-    public char GetLetter(int n) {
-        Contract.Requires(n < 9);
-        return "milksteak"[n];
-    }
-
-    public char GetLetter() {
-        return GetLetter(-1);
-    }
-
-}
 
 }
