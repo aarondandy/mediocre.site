b985eee9e9f8dac4e8126548949e1501ed757c87
Aaron Dandy <aaron.dandy@live.com>
2016-10-09 12:48:50 -0700

    Start a new workspace for a custom wyam module

--- MediocreSite.Wyam/Properties/AssemblyInfo.cs
+++ MediocreSite.Wyam/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VertesaurSays")]
+[assembly: AssemblyTitle("MediocreSite.Wyam")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VertesaurSays")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("MediocreSite.Wyam")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("812c2ae1-fcec-4383-a0b2-2eedaea5e375")]
+[assembly: Guid("71874172-2609-4c55-9358-aad0460362e2")]
 
 // Version information for an assembly consists of the following four values:
 //
